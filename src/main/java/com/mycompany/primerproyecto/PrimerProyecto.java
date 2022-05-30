986e3cfc911c32bd14c58ff835b19bedceb01021
Jose20796 <jose_brice_bas1996@outlook.com>
2022-05-30 12:40:36 -0500

    Netbeans

---
+++ src/main/java/com/mycompany/primerproyecto/PrimerProyecto.java
@@ -0,0 +1,16 @@
+package com.mycompany.primerproyecto;
+
+import controlador.Prueba;
+
+/**
+ *
+ * @author matprog06
+ */
+public class PrimerProyecto {
+
+    public static void main(String[] args) {
+        Prueba prueba=new Prueba();
+        prueba.saludo();
+        prueba.despedida();
+    }
+}
