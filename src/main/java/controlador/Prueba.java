420b48bfbb7a2c126ca4f8c2aba072d444de75f3
Jose20796 <jose_brice_bas1996@outlook.com>
2022-05-30 13:15:09 -0500

    Desde la PC

---
+++ src/main/java/controlador/Prueba.java
@@ -0,0 +1,26 @@
+
+package controlador;
+
+/**
+ *
+ * @author matprog06
+ */
+public class Prueba {
+    
+    public void saludo(){
+        System.out.println("Hola");
+    }
+    public void despedida(){
+        System.out.println("Adiós");
+    }
+    public void contar(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i+"\n");
+        }
+    }
+    public void contar2(){
+        for (int i = 0; i < 20; i++) {
+            System.out.println(i+"\n");
+        }
+    }
+}
